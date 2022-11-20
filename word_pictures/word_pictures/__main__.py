44ac7558814e0cb614bbdf485415ea75d2202299
bmm514 <benmurcott96@gmail.com>
2022-11-20 15:14:19 +0000

    Converting a string of words to picture complete

--- word_pictures/word_pictures/__main__.py
+++ word_pictures/word_pictures/__main__.py
@@ -1,9 +1,15 @@
 from . import converter
+from . import words
 
 def main():
-    ascii_string = 'The quick brown fox jumped over the lazy cat'
+    ascii_string = 'The quick brown fox jumped over the lazy cat'*1000
+    ascii_string = words.mit_10000_words
+    ascii_string = 'age'*9
+    square_size_mm = 10
     hex_string = converter.ascii_to_hex(ascii_string)
     hex_colors, hex_string_length = converter.make_hex_colors(hex_string)
-    square_hex_colors = converter.make_color_square(hex_colors, hex_string_length)
+    square_hex_colors, square_side = converter.make_color_square(hex_colors, hex_string_length)
+    coord_array = converter.make_coordinate_array(square_size_mm, square_side)
+    data = converter.test_image(coord_array, square_hex_colors)
 
-    return hex_string, hex_colors, square_hex_colors
+    return hex_string, hex_colors, square_hex_colors, coord_array, data
--- word_pictures/word_pictures/converter.py
+++ word_pictures/word_pictures/converter.py
@@ -1,6 +1,7 @@
 from math import sqrt
 
 import numpy
+from PIL import Image, ImageColor
 
 # convert ascii string to hex
 
@@ -29,7 +30,7 @@ def make_hex_colors(hex_string):
     hex_colors = []
     for i in range(hex_string_length):
         lower, upper = (6*i, 6*i+6)
-        hex_colors.append(hex_string[lower:upper])
+        hex_colors.extend(['#' + hex_string[lower:upper]])
 
     hex_colors = numpy.array(hex_colors)
 
@@ -47,6 +48,48 @@ def make_color_square(hex_colors, hex_string_length):
     square_side, array_length = square_size(hex_string_length)
     square_hex_colors = hex_colors[:array_length].reshape((square_side, square_side))
 
-    return square_hex_colors
+    return square_hex_colors, square_side
+
+def mm_to_pixel(size):
+    conversion = 3.7795275591
+    return int(conversion * size)
+
+#Use the square_hex_colors
+#Create another array containing coordinates in the same shape!
+#Go through both together filling in the colors
+def make_coordinate_array(square_size_mm, square_side):
+    square_size_pixel = mm_to_pixel(square_size_mm)
+    coord_array = numpy.zeros((square_side, square_side, 4), int)
+    for x, _ in enumerate(coord_array):
+        for y, _ in enumerate(coord_array):
+            coord_array[y,x] = [x*square_size_pixel, y*square_size_pixel, (x+1)*square_size_pixel, (y+1)*square_size_pixel]
+
+    return coord_array
+
+
+def test_image(coord_array, square_hex_colors):
+    width = coord_array[-1][-1][2]
+    height = coord_array[-1][-1][3]
+    data = numpy.zeros((height, width, 3), dtype = numpy.uint8)
+    data[:,:] = [255,255,255] 
+    for x, col in enumerate(coord_array):
+        for y, val in enumerate(col):
+            a, b, c, d = val
+            color = square_hex_colors[x][y]
+            data[a:c, b:d] = ImageColor.getrgb(color)
+
+    img = Image.fromarray(data)
+    img.save('fig2.png')
+
+    return data
 
 # output square as picture
+def make_image(square_hex_colors, square_size = 10):
+    square_size_pixel = mm_to_pixel(square_size)
+
+    array = numpy.arange(0, 737280, 1, numpy.uint8)
+    array = numpy.reshape(array, (1024, 720))
+    print(array)
+
+    data = Image.fromarray(array)
+    data.save('test.png')
