bb64b2dfcf2ce34db804e41fbd79529b3def2739
bmm514 <benmurcott96@gmail.com>
2022-11-20 15:16:14 +0000

    Changing string to longer word

--- word_pictures/word_pictures/__main__.py
+++ word_pictures/word_pictures/__main__.py
@@ -4,7 +4,7 @@ from . import words
 def main():
     ascii_string = 'The quick brown fox jumped over the lazy cat'*1000
     ascii_string = words.mit_10000_words
-    ascii_string = 'age'*9
+#    ascii_string = 'age'*9
     square_size_mm = 10
     hex_string = converter.ascii_to_hex(ascii_string)
     hex_colors, hex_string_length = converter.make_hex_colors(hex_string)
