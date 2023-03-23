9d43986cfb3622d94c39f20ed6fb8fd89bd50de3
Ben <bmm41@bath.ac.uk>
2023-03-23 12:03:24 +0000

    Adding argparse to track_xp

--- pokemonGO/track_xp.py
+++ pokemonGO/track_xp.py
@@ -52,7 +52,10 @@ class pokemonGOtracker:
                 replace_xp = 'y'
 
             if replace_xp == 'y':
-                tracker_json.update({date : xp})
+                tracker_json[date] = xp
+                print(f'Updated xp for {date}')
+            else:
+                print('Not updated')
 
         with open(self.tracker_file, 'w') as jsonout:
             json.dump(tracker_json, jsonout, indent = 2)
@@ -63,21 +66,36 @@ def main():
     from argparse import ArgumentParser
 
     parser = ArgumentParser(description = 'PokemonGo tracker for xp, ...')
+    parser.add_argument('--force_yes', help = 'Always answer yes to questions', action = 'store_true')
 
-    update_parser = parser.add_argument_group('update', description = 'Update xp')
+    subparser = parser.add_subparsers(title = 'command', description = 'Choose mode to run in:', dest = 'command', required = True)
+    update_parser = subparser.add_parser('update', help = 'Update XP for a date')
     update_parser.add_argument('xp', help = 'Xp value to add to the data storage for the date specified')
-    update_parser.add_argument('--date', help = 'Date for the Xp value to be added to')
+    update_parser.add_argument('--date', help = 'Date for the Xp value to be added to. Default to current date')
 
+    args = parser.parse_args()
 
+    xp = args.xp
+    date = args.date
+    accept_all = args.force_yes
 
-if __name__ == '__main__':
+    print(xp, date, accept_all)
+
     tracker_file = 'test/bmm.json'
 
     pokeGOtracker = pokemonGOtracker(tracker_file) 
+    pokeGOtracker.update_xp(xp, date, accept_all)
 
-    print(pokeGOtracker.tracker_file)
-    print(pokeGOtracker.directory)
-    print(pokeGOtracker.update_xp(5924750, '20/03/23'))
-    print(pokeGOtracker.update_xp(5976650, '21/03/23'))
-    print(pokeGOtracker.update_xp(6118500, '22/03/23'))
+
+if __name__ == '__main__':
+    main()
+#    tracker_file = 'test/bmm.json'
+#
+#    pokeGOtracker = pokemonGOtracker(tracker_file) 
+#
+#    print(pokeGOtracker.tracker_file)
+#    print(pokeGOtracker.directory)
+#    print(pokeGOtracker.update_xp(5924750, '20/03/23'))
+#    print(pokeGOtracker.update_xp(5976650, '21/03/23'))
+#    print(pokeGOtracker.update_xp(6118500, '22/03/23'))
 
