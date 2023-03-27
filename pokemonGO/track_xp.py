3ac8c8e4dfb38c1dbbd7728d3015a48c735ad50f
Ben <bmm41@bath.ac.uk>
2023-03-27 10:45:20 +0100

    File initialisation restructuring and delete function added

--- pokemonGO/track_xp.py
+++ pokemonGO/track_xp.py
@@ -18,13 +18,27 @@ class pokemonGOtracker:
                 makedirs(self.directory)
             
             try:
-                open(self.tracker_file, 'a').close()
+                with open(self.tracker_file, 'a') as jsonin:
+                    self._open_json(jsonin)
             except OSError:
                 print('Failed creating the file')
             finally:
                 print('Successfully initialised the file')
         else:
+            with open(self.tracker_file) as jsonin:
+                self._open_json(jsonin)
             print('Successfully opened the file')
+
+    def _open_json(self, jsonin):
+        try:
+            self.tracker_json = OrderedDict(json.load(jsonin))
+            print('Opened without JSONDecodeError')
+        except json.decoder.JSONDecodeError:
+            self.tracker_json = OrderedDict()
+            print('Opened with JSONDecoreError')
+    def _save_json(self):
+        with open(self.tracker_file, 'w') as jsonout:
+            json.dump(self.tracker_json, jsonout, indent = 2)
 
     def _clear_tracker(self):
         try:
@@ -37,34 +51,31 @@ class pokemonGOtracker:
     def update_xp(self, xp, date, accept_all = False):
         '''Add the XP on a date specified in the format DD/MM/YY'''
         try:
-            tracker_json = OrderedDict(json.load(open(self.tracker_file)))
-            if date in tracker_json.keys():
+            #tracker_json = OrderedDict(json.load(open(self.tracker_file)))
+            if date in self.tracker_json.keys():
                 raise AlreadyInDict
-            tracker_json.update({date : xp})
-        #except JSONDecodeError:
-        except json.decoder.JSONDecodeError:
-            tracker_json = OrderedDict({date : xp})
+            self.tracker_json.update({date : xp})
+        #except json.decoder.JSONDecodeError:
+            #self.tracker_json = OrderedDict({date : xp})
         except AlreadyInDict:
-            current_xp = tracker_json[date]
+            current_xp = self.tracker_json[date]
             if not accept_all:
                 replace_xp = input(f'Replace the current xp, {current_xp}, with {xp}? y/n?')
             else:
                 replace_xp = 'y'
 
             if replace_xp == 'y':
-                tracker_json[date] = xp
+                self.tracker_json[date] = xp
                 print(f'Updated xp for {date}')
             else:
                 print('Not updated')
 
-        with open(self.tracker_file, 'w') as jsonout:
-            json.dump(tracker_json, jsonout, indent = 2)
+        self._save_json()
         
-        return(tracker_json)
-    
     def delete_record(self, date):
         '''Delete a record from the database'''
-        pass
+        del self.tracker_json[date]
+        self._save_json()
 
 def update_main(args):
     tracker_file = args.tracker_json
@@ -75,7 +86,12 @@ def update_main(args):
     pokeGOtracker = pokemonGOtracker(tracker_file) 
     pokeGOtracker.update_xp(xp, date, accept_all)
 
+def delete_main(args):
+    tracker_file = args.tracker_json
+    date = args.date
 
+    pokeGOtracker = pokemonGOtracker(tracker_file) 
+    pokeGOtracker.delete_record(date)
 
 def main():
     from argparse import ArgumentParser
@@ -90,13 +106,11 @@ def main():
     update_parser.add_argument('--date', help = 'Date for the Xp value to be added to. Default to current date')
     update_parser.set_defaults(func=update_main)
 
+    delete_parser = subparser.add_parser('delete', help = 'Delete a date record')
+    delete_parser.add_argument('--date', help = 'Date for the Xp value to be deleted.')
+    delete_parser.set_defaults(func=delete_main)
+
     args = parser.parse_args()
-
-    xp = args.xp
-    date = args.date
-    accept_all = args.force_yes
-
-    print(xp, date, accept_all)
 
     args.func(args)
 
