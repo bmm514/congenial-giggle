b3ff51ebdcad472b41558c29c1c0a11960685af8
Ben <bmm41@bath.ac.uk>
2023-04-25 09:53:47 +0100

    Simple Updating xp

--- pokemonGO/track_xp_test.py
+++ pokemonGO/track_xp_test.py
@@ -7,9 +7,11 @@ def create_db(database):
     return connection, cursor
 
 class pokemonGOtracker:
-    def __init__(self, database):
+    def __init__(self, database, username):
         self._connection = sqlite3.connect(database)
         self._cursor = self._connection.cursor()
+        self._username = username
+        self._database = database
 
     def __enter__(self):
         return self
@@ -24,6 +26,14 @@ class pokemonGOtracker:
     @property
     def cursor(self):
         return self._cursor
+
+    @property
+    def username(self):
+        return self._username
+
+    @property
+    def database(self):
+        return self._database
 
     def commit(self):
         self.connection.commit()
@@ -45,13 +55,14 @@ class pokemonGOtracker:
 
     def update_xp(self, xp, date, accept_all = False):
         """Add the CP on a date specified in isoformat (YYYY-MM-DD)"""
-        #Use similar code from json example
+        self.execute("""INSERT INTO xp_tracker VALUES (:name, :date, :date)""", 
+                {'database' : self.database, 'name' : self.username, 'date' : date, 'xp' : xp})
         pass
 
 def main():
     database = ':memory:'
 
-    pokeGOtracker = pokemonGOtracker(database)
+    pokeGOtracker = pokemonGOtracker(database, 'bmm')
 
     pokeGOtracker.execute("""
         CREATE TABLE xp_tracker (
@@ -62,8 +73,9 @@ def main():
         """
         )
 
-    pokeGOtracker.execute("""INSERT INTO xp_tracker VALUES ('bmm', '2023-04-23', 10000)""")
-    pokeGOtracker.execute("""INSERT INTO xp_tracker VALUES ('bmm', '2023-04-24', 20000)""")
+    pokeGOtracker.update_xp(10000, '2023-04-23')
+    pokeGOtracker.update_xp(20000, '2023-04-24')
+    pokeGOtracker.update_xp(25000, '2023-04-25')
     print('Added values')
     rows = pokeGOtracker.query("""SELECT * FROM xp_tracker WHERE name='bmm' ORDER BY date DESC""")
     print(rows)
