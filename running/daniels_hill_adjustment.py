c50d4588efa1ab33201760ec0b60d54f3e1c6d75
Ben <bmm41@bath.ac.uk>
2023-04-26 10:52:10 +0100

    Times for TEC BB added in csv

--- running/daniels_hill_adjustment.py
+++ running/daniels_hill_adjustment.py
@@ -1,3 +1,4 @@
+import csv
 from collections import defaultdict
 import datetime
 
@@ -15,7 +16,7 @@ def compare_cp_times(checkpoint_info, flat_paces):
     for flat_pace in flat_paces:
         total_time = 0
         for x, (miles, uphill, downhill) in enumerate(checkpoint_info):
-            cp_time = calc_time(miles, uphill, downhill, flat_pace)
+            cp_time = calc_time(miles, uphill + 500/6, downhill + 500 / 6, flat_pace)
             total_time += cp_time
             pace_cp_times[flat_pace][f'CP{x+1}'] = (cp_time, total_time)
 
@@ -29,9 +30,35 @@ def print_times(pace_cp_times):
             total_time = str(datetime.timedelta(minutes = total_time))
             print(f'Check Point {cp}; from last: {cp_time}, total: {total_time}')
 
+def write_to_csv(pace_cp_times, cp_fileout, total_fileout):
+    with open(cp_fileout, 'w') as cp_fout, open(total_fileout, 'w') as t_fout:
+        cp_csvwriter = csv.writer(cp_fout, delimiter = ',')
+        t_csvwriter = csv.writer(t_fout, delimiter = ',')
+        header = ['flat_pace']
+        cp_rows = []
+        t_rows = []
+        for flat_pace, values in pace_cp_times.items():
+            header_extension = values.keys()
+            cp_row = [flat_pace]
+            t_row = [flat_pace]
+            for cp, (cp_time, total_time) in values.items():
+                cp_row.append(str(datetime.timedelta(minutes = cp_time)))
+                t_row.append(str(datetime.timedelta(minutes = total_time)))
+
+            cp_rows.append(cp_row)
+            t_rows.append(t_row)
+        
+        header.extend(list(header_extension))
+        cp_csvwriter.writerow(header)
+        cp_csvwriter.writerows(cp_rows)
+        t_csvwriter.writerow(header)
+        t_csvwriter.writerows(t_rows)
+
 
 if __name__ == '__main__':
     checkpoint_info = [(2.3, 166, 13), (4.9, 319, 175), (6.7, 457, 438), (6.7, 331, 473), (9.1, 668, 727), (3.1, 14, 254)]
 
+
     pace_cp_times = compare_cp_times(checkpoint_info, [7.5, 8, 8.5, 9, 9.5, 10])
     print_times(pace_cp_times)
+    write_to_csv(pace_cp_times, 'tecBB_cp_times.csv', 'tecBB_total_times.csv')
